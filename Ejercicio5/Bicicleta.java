3727c11c42964bf99821487d7aaecd2a018799e8
pbarlar <pbarlar@g.educaand.es>
2025-01-09 14:26:45 +0100

    create

---
+++ Ejercicio5/Bicicleta.java
@@ -0,0 +1,8 @@
+package Ejercicio5;
+
+public class Bicicleta {
+    public static int velocidad=0;
+    public static final String color="Negro";
+    public static int marcha=2;
+
+}
---
+++ Ejercicio5/CrearBicicleta.java
@@ -0,0 +1,8 @@
+package Ejercicio5;
+
+class CrearBicicleta {
+    public static void main(String[] args) {
+        Bicicleta miBicicleta=new Bicicleta();
+    }
+    
+}
