5f226a81d35b9faaf03d27ce0bb41ec80c9d58eb
pbarlar <pbarlar@g.educaand.es>
2025-01-22 14:17:52 +0100

    Update ComprarEntradas.java

--- Ejercicio8/ComprarEntradas.java
+++ Ejercicio8/ComprarEntradas.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class ComprarEntradas {
     
     public static void main(String[] args) {
-        Zona z1=new Zona(1225);
+        Zona principal=new Zona(1000);
+        Zona compra_venta=new Zona(200);
+        Zona vip=new Zona(25);
         Scanner entrada=new Scanner(System.in);
-        int opcion=0,i=0;
+        int opcion=0, comprar=0, zona=0;
 
-        for (i = 0; i == 3;) {
+        while (opcion!=3) {
             System.out.println("1. Mostrar numero de entradas libres.");
             System.out.println("2. Vender entradas.");
             System.out.println("3. Salir.");
@@ -17,13 +19,25 @@ public class ComprarEntradas {
             opcion=entrada.nextInt();
             switch (opcion) {
                 case 1:
-                    System.out.println("Quedan "+z1.getEntradasPorVender()+" entradas por vender ");
+                    System.out.println("Quedan "+(principal.getEntradasPorVender()+compra_venta.getEntradasPorVender()+vip.getEntradasPorVender())+" entradas por vender ");
+                    System.out.println();
                     break;
                 case 2:
-                    z1.vender(30);
-                    break;
-                case 3:
-                    i=3;
+                    System.out.println("De que zona quieres comprar: ");
+                    System.out.println("1. Zona principal.");
+                    System.out.println("2. Zona compra-venta.");
+                    System.out.println("3. Zona vip");
+                    System.out.println("Eliga una opción: ");
+                    zona=entrada.nextInt();
+                    System.out.println("Cuantas entradas quieres comprar: ");
+                    comprar=entrada.nextInt();
+                    if (opcion==1){
+                        principal.vender(comprar);
+                    } else if (opcion==2){
+                        compra_venta.vender(comprar);
+                    }else if (opcion==3){
+                        vip.vender(comprar);
+                    }
                     break;
             }
         }
